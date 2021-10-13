91815a4c9f10982f3b0a56aa574356b096635254
blosloos <blosloos@yahoo.de>
2021-10-13 11:02:34 +0200

    initialize package

---
+++ Swisssfm/inst/develope_Swisssfm.r
@@ -0,0 +1,8 @@
+
+
+package.skeleton(name = "Swisssfm", 
+                 environment = .GlobalEnv,
+                 path = "C:/PART_4/MS/R_packages/Swisssfm", force = FALSE,
+                 code_files = character(), encoding = "unknown")
+				 
+				 
