c1d8830b5a4f9a4f9f3bf5dd04107cc89ce77877
blosloos <mloos@envibee.ch>
2021-10-19 12:08:09 +0200

    Anpassungen

--- Swisssfm/R/make_topology.r
+++ Swisssfm/R/make_topology.r
@@ -6,29 +6,31 @@
 
 
 make_topology <- function(
-	ara_id_next, 					# NA if none available
-	ara_id = FALSE,					
-	NA_next_ignore = FALSE			# ara_id_next not in ara_id? -> set to NA as well
+	STP_id_next, 					# NA if none available
+	STP_id = FALSE,					
+	NA_next_ignore = FALSE,			# ara_id_next not in STP_id? -> set to NA as well
+	insert_id_in_topo_matrix = FALSE
 ){
 
 	###############################################
 	# check inputs & defaults 	
-	len <- length(ara_id_next)
-	if(isFALSE(ara_id[1])) ara_id <- seq(len) else{ 
-		if(length(ara_id) != len) stop("Problem in make_topology: ara_id_next and ara_id must be of same length") 
-		if(any(is.na(ara_id))) stop("Problem in make_topology: ara_id must not contain NAs") 
+	len <- length(STP_id_next)
+	if(isFALSE(STP_id[1])) STP_id <- seq(len) else{ 
+		if(length(STP_id) != len) stop("Problem in make_topology: STP_id_next and STP_id must be of same length") 
+		if(any(is.na(STP_id))) stop("Problem in make_topology: STP_id must not contain NAs") 
 	}
 	if(NA_next_ignore){
-		if(any(!(ara_id_next[!is.na(ara_id_next)] %in% ara_id))) stop("Problem in make_topology: ara_id and ara_id_next mismatching")
-	}else ara_id_next[!(ara_id_next %in% ara_id)] <- NA
+		if(any(!(STP_id_next[!is.na(STP_id_next)] %in% STP_id))) stop("Problem in make_topology: STP_id and STP_id_next mismatching")
+	}else STP_id_next[!(STP_id_next %in% STP_id)] <- NA
 	###############################################
 	bin_link_matrix <- matrix(nrow = len, ncol = len, 0)
-	ara_nr_next <- match(ara_id_next, ara_id)				# NAs returned
-	for(i in 1:len) bin_link_matrix[i, ara_nr_next[i]] <- 1 # NAs skipped
-	topo_matrix <- solve(diag(len) - bin_link_matrix)
-	colnames(topo_matrix) <- rownames(topo_matrix) <- ara_id
+	STP_nr_next <- match(STP_id_next, STP_id)					# NAs returned
+	for(i in 1:len) bin_link_matrix[i, STP_nr_next[i]] <- 1 	# NAs skipped
+	topo_matrix <- solve(diag(len) - bin_link_matrix)			# 0 or 1
+	colnames(topo_matrix) <- rownames(topo_matrix) <- STP_id
+	if(isTRUE(insert_id_in_topo_matrix)) for(i in ncol(topo_matrix)) topo_matrix[, i] <- topo_matrix[, i] * STP_id
 	###############################################
-	return(topo_matrix)
+	return(topo_matrix) # columns mark upstream STPs of each STP
 	
 }
 
--- Swisssfm/R/run_daily_load.r
+++ Swisssfm/R/run_daily_load.r
@@ -7,36 +7,39 @@
 
 run_daily_load <- function(
 
-	einwohner_total = 8417700,
-	spitalbetten_total = FALSE,
-	
-	ara_id, 
-	ara_anteil_spital,
-	ara_anzahl_einwohner,
-	ara_anzahl_spitalbetten = FALSE,
-	
-	stoff_menge_total, 			# [kg / a]
-	stoff_elimination_ara, 
-	stoff_menge_in_kanal,		# fraction excreted and discharged
-	
+	inhabitants_total = 8417700,
+	hospital_beds_total = FALSE,				# Set to FALSE to ignore
+	STP_id, 
+	STP_fraction_hospital,
+	STP_amount_inhabitants,	
+	STP_amount_hospital_beds = FALSE,			# Set to FALSE to ignore
+	compound_load_total, 						# [kg / a]
+	compound_load_per_capita_and_day = FALSE,	# [g / d], set to FALSE to ignore
+	compound_elimination_ara,					# vector with elimination fractions over treatment steps (not percentage values); set to 0 to skip a step 
+	compound_excreted = 1,						# fraction excreted and discharged, set to 1 to ignore
 	topo_matrix
+	
 ){
 
 	###############################################
 	# check inputs & defaults
-	if(!all(ara_id %in% colnames(topo_matrix))) stop("Problem in run_daily_load: not all ara_id present in topo_matrix")
-	if(!identical(length(ara_id), length(ara_anteil_spital), length(ara_anzahl_einwohner))) stop("Problem in run_daily_load: ara inputs vary in length_1")
-	if(isFALSE(ara_anzahl_spitalbetten)) ara_anzahl_spitalbetten <- 0 else if(length(ara_anzahl_spitalbetten) != length(ara_id)) stop("Problem in run_daily_load: ara inputs vary in length_2")
-	if(!identical(length(stoff_menge_total), length(stoff_elimination_ara), length(stoff_menge_in_kanal))) stop("Problem in run_daily_load: stoff inputs vary in length")
-	if(isFALSE(spitalbetten_total)) spitalbetten_total <- 0
+	if(!all(STP_id %in% colnames(topo_matrix))) stop("Problem in run_daily_load: not all STP_id present in topo_matrix")
+	if(!all(colnames(topo_matrix) %in% STP_id)) stop("Problem in run_daily_load: not all topo_matrix entries present in STP_id")	
+	if(!identical(length(STP_id), length(STP_fraction_hospital), length(STP_amount_inhabitants))) stop("Problem in run_daily_load: ara inputs vary in length_1")
+	if(isFALSE(STP_amount_hospital_beds)) STP_amount_hospital_beds <- 0 else if(length(STP_amount_hospital_beds) != length(STP_id)) stop("Problem in run_daily_load: ara inputs vary in length_2")
+	if(!identical(length(compound_load_total), length(compound_elimination_ara), length(compound_excreted))) stop("Problem in run_daily_load: stoff inputs vary in length")
+	if(isFALSE(hospital_beds_total)) hospital_beds_total <- 0
+	if(any((compound_elimination_ara < 0) & (compound_elimination_ara > 1))) stop("Problem in run_daily_load: compound_elimination_ara not in all [0,1]")
+	topo_matrix[topo_matrix != 0] <- 1 	# in case topo_matrix contains STP id
 	###############################################
-	menge_pro_kopf_und_tag_total <- stoff_menge_total * (1 - ara_anteil_spital) * 1000 / einwohner_total / 365 		# [kg/a] -> [g/d]
-	menge_pro_spitalbett_und_tag_total <- stoff_menge_total * (ara_anteil_spital) * 1000 / spitalbetten_total / 365 	# [kg/a] -> [g/d]
-	lokal <- ara_anzahl_einwohner * menge_pro_kopf_und_tag_total * stoff_menge_in_kanal * (1 - stoff_elimination_ara)
-	lokal <- lokal + ara_anzahl_spitalbetten * menge_pro_spitalbett_und_tag_total * stoff_menge_in_kanal * (1 - stoff_elimination_ara)
-	kummuliert <- apply(topo_matrix, MARGIN = 2, function(x){sum(x * lokal)}, y = lokal)
+	if(isFALSE(compound_load_per_capita_and_day)) compound_load_per_capita_and_day <- compound_load_total * (1 - STP_fraction_hospital) * 1000 / inhabitants_total / 365 		# [kg/a] -> [g/d]
+	compound_load_per_hospital_bed_and_day <- compound_load_total * (STP_fraction_hospital) * 1000 / hospital_beds_total / 365 	# [kg/a] -> [g/d]	
+	compound_elimination_ara <- prod(1 - compound_elimination_ara) # prod: if several steps combined
+	load_local <- STP_amount_inhabitants * compound_load_per_capita_and_day * compound_excreted * compound_elimination_ara
+	load_local <- load_local + STP_amount_hospital_beds * compound_load_per_hospital_bed_and_day * compound_excreted * compound_elimination_ara
+	load_cumulated <- apply(topo_matrix, MARGIN = 2, function(x){sum(x * load_local)}, y = load_local) 
 	###############################################
-	return(cbind(ara_id, lokal, kummuliert) 
+	return(cbind(STP_id, lokal, load_cumulated) 
 	
 }
 
